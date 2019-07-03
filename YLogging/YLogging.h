c7caf337470c7a56339161bf38695242775efb53
Rajasekar <gee.rajasekar@gmail.com>
2019-07-03 15:01:53 +0530

    Initial Commit

---
+++ YLogging/YLogging.h
@@ -0,0 +1,19 @@
+//
+//  YLogging.h
+//  YLogging
+//
+//  Created by CVN on 03/07/19.
+//  Copyright © 2019 Agiliztech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YLogging.
+FOUNDATION_EXPORT double YLoggingVersionNumber;
+
+//! Project version string for YLogging.
+FOUNDATION_EXPORT const unsigned char YLoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YLogging/PublicHeader.h>
+
+
